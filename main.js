9188332fdb1dcf04c48fd6562c981f53b87443d8
Mehul Jadeja <mehul.jadeja@kaizen.co.uk>
2020-03-19 00:01:13 +0000

    math functions and operators

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const calculator = { result: '0', initialIn: null, secondOp: false }
+const performCalculation = {
+  '/': (initialIn, secondIn) => initialIn / secondIn,
+  '*': (initialIn, secondIn) => initialIn * secondIn,
+  '+': (initialIn, secondIn) => initialIn + secondIn,
+  '-': (initialIn, secondIn) => initialIn - secondIn,
+  '=': (initialIn, secondIn) => secondIn
+}
+
 function updateDisplay() {
   const display = document.querySelector('.result')
   display.value = calculator.result.includes(',') ? (+(calculator.result.replace(/,/g, ''))).toLocaleString() : (+calculator.result).toLocaleString()
@@ -10,6 +18,21 @@ const keys = document.querySelector('.calc')
 keys.addEventListener('click', (event) => {
   const { target } = event
   if (!target.matches('button')) {
+    return
+  }
+  if (target.classList.contains('mathbutton')) {
+    doMath(target.value)
+    updateDisplay()
+    return
+  }
+  if (target.classList.contains('decimal')) {
+    point(target.value)
+    updateDisplay()
+    return
+  }
+  if (target.classList.contains('all-clear')) {
+    resetCalculator()
+    updateDisplay()
     return
   }
   input(target.value)
@@ -24,4 +47,38 @@ function input(digit) {
   } else {
     calculator.result = result === '0' ? digit : result + digit
   }
+}
+
+function point(dot) {
+  if (calculator.secondOp === true) return
+  if (!calculator.result.includes(dot)) { calculator.result += dot }
+}
+
+function doMath(nextOperator) {
+  const { initialIn, result, operator } = calculator
+  const inputValue = parseFloat(result)
+
+  if (operator && calculator.secondOp) {
+    calculator.operator = nextOperator
+    return
+  }
+  if (initialIn == null) {
+    calculator.initialIn = inputValue
+  } else if (operator) {
+    const currentValue = initialIn || 0
+    const result = performCalculation[operator](currentValue, inputValue)
+    calculator.result = String(result.toLocaleString())
+    calculator.initialIn = result
+  }
+
+  calculator.secondOp = true
+  calculator.operator = nextOperator
+}
+
+
+function resetCalculator() {
+  calculator.result = '0'
+  calculator.initialIn = null
+  calculator.secondOp = false
+  calculator.operator = null
 }
